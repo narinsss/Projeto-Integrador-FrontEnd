231d5243506521e8ab9ac79b500c07691ca039a2
narinsss <maarib03@gmail.com>
2022-09-27 10:24:57 -0300

    troca de localhost para o link do heroku

--- src/app/service/produto.service.ts
+++ src/app/service/produto.service.ts
@@ -17,28 +17,28 @@ export class ProdutoService {
   }
 
   getAllProduto(): Observable<Produto[]> {
-    return this.http.get<Produto[]>('http://localhost:8080/produto', this.token)
+    return this.http.get<Produto[]>('https://acquacode.herokuapp.com/produto', this.token)
   }
 
   getByIdProduto(id: number): Observable<Produto>{
-    return this.http.get<Produto>(`http://localhost:8080/produto/${id}`, this.token)
+    return this.http.get<Produto>(`https://acquacode.herokuapp.com/produto/${id}`, this.token)
   }
 
   getByNomeProduto(nomeProduto: string): Observable<Produto[]>{
-    return this.http.get<Produto[]>(`http://localhost:8080/produto/${nomeProduto}`, this.token)
+    return this.http.get<Produto[]>(`https://acquacode.herokuapp.com/produto/${nomeProduto}`, this.token)
   }
 
 
   postProduto(produto: Produto): Observable<Produto>{
-    return this.http.post<Produto>('http://localhost:8080/produto', produto, this.token)
+    return this.http.post<Produto>('https://acquacode.herokuapp.com/produto', produto, this.token)
   }
 
   putProduto(produto: Produto): Observable<Produto>{
-    return this.http.put<Produto>('http://localhost:8080/produto', produto, this.token)
+    return this.http.put<Produto>('https://acquacode.herokuapp.com/produto', produto, this.token)
   }
 
   deleteProduto(id: number){
-    return this.http.delete(`http://localhost:8080/produto/${id}`, this.token)
+    return this.http.delete(`https://acquacode.herokuapp.com/produto/${id}`, this.token)
   }
 
 }
